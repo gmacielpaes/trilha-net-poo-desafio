eec1d37a79927c5b0b00ecebf832b5df890e8d78
Gabriel Maciel Paes <gmacielpaes@gmail.com>
2024-01-08 09:21:22 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "84993324587", modelo: "Modelo 1", imei: "123123123", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n---------------------------------\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "81999336728", modelo: "Modelo 2", imei: "345345345", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iFood");
+
+Console.WriteLine("\n");
